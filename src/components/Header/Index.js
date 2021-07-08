fa48470050cca993ba5df41b56ee1305e5f3b4d9
Tiago Lindgren Curi <shopper.tiago@gmail.com>
2021-07-07 21:27:51 -0300

    corrigindo erro

--- src/components/Header/Index.js
+++ src/components/Header/Index.js
@@ -26,7 +26,7 @@ const Header = () => {
                     ) : (
                         <>
                             <div>
-                                <a href="#">Mudar loja</a>
+                                <a href="/">Mudar loja</a>
                             </div>
                             <div>
                                 <span>Fazendo pedidos na loja Tijuca</span>
